21da44909b11ff1ae4bd124fdb6a62d0c4a6c9af
Clara Andrew <candrewwani@gmail.com>
2018-05-02 18:08:25 -0400

    guided demo

--- guided-demo/cca/src/App.js
+++ guided-demo/cca/src/App.js
@@ -23,24 +23,71 @@ class CCACanvas extends Component {
    */
   constructor(props) {
     super(props);
+
+    this.cca = new CCA(props.width, props.height);
+    this.cca.randomize();
   }
 
   /**
    * Component did mount
    */
   componentDidMount() {
+    requestAnimationFrame(() => this.animFrame());
   }
 
   /**
    * Handle an animation frame
    */
   animFrame() {
+
+    this.cca.step();
+    let width = this.props.width;
+    let height = this.props.height;
+
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, width, height);
+    ctx.fill();
+
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    // for(let pixel of imageData.data)
+    // {
+    //   pixel = 128;
+    //   //console.log(pixel);
+    // }
+    
+    
+    // blueComponent = imageData.data[((50 * (imageData.width * 4)) + (200 * 4)) + 2];
+    
+    for(let row = 0; row < height; row++){
+      for(let col = 0; col < height; col++){
+        let ccaGrid = this.cca.getCells();
+        let index = (row * width + col) * 4;
+        let color = ccaGrid[row][col];
+        //change red value
+        imageData.data[index + 0] =  COLORS[color][0];
+        //change green value
+        imageData.data[index + 1] =  COLORS[color][1];
+        //change blue value
+        imageData.data[index + 2] =  COLORS[color][2];
+        
+        imageData.data[index + 3] =  0xff; //alpha channel solid
+      }
+    }
+
+    //console.log("imageData", imageData);
+    ctx.putImageData(imageData, 0, 0);
+    requestAnimationFrame(() => this.animFrame());
   }
 
   /**
    * Render
    */
   render() {
+    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
   }
 }
 
